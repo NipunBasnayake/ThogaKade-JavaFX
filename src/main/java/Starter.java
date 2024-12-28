4e829a4c0345027e787fe4b474782f757e676f71
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-12-28 22:39:45 +0530

    Customer CRUD Completed

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -7,9 +7,11 @@ public class Starter extends Application {
     public static void main(String[] args) {
         launch();
     }
+
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer-form.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/home-page.fxml"))));
         stage.show();
     }
+
 }

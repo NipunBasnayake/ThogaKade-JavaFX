f47b7e45c5ba89533da94f85d669609679a02cd6
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2025-01-12 10:12:19 +0530

    Customer Controller Bugs Fixed

--- src/main/java/controller/customer/CustomerController.java
+++ src/main/java/controller/customer/CustomerController.java
@@ -16,13 +16,38 @@ public class CustomerController implements CustomerServices {
 
     @Override
     public boolean addCustomer(Customer customer) {
-        PreparedStatement statement = null;
-        try {
-            statement = DBConnection.getInstance().getConnection().prepareStatement("INSERT INTO customer VALUES (?,?,?,?)");
-            statement.setString(1, customer.getId());
-            statement.setString(2, customer.getName());
-            statement.setString(3, customer.getAddress());
-            statement.setDouble(4, customer.getSalary());
+        String sql = "INSERT INTO customer VALUES (?,?,?,?)";
+
+        String customerId = customer.getId();
+        String customerName = customer.getName();
+        String customerAddress = customer.getAddress();
+        double customerSalary = customer.getSalary();
+
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, customerId);
+            statement.setString(2, customerName);
+            statement.setString(3, customerAddress);
+            statement.setDouble(4, customerSalary);
+
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to add customer", e);
+        }
+    }
+
+
+    @Override
+    public boolean updateCustomer(Customer customer) {
+        String sql = "UPDATE customer SET name=?,address=?,salary=? WHERE id=?";
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, customer.getName());
+            statement.setString(2, customer.getAddress());
+            statement.setDouble(3, customer.getSalary());
+            statement.setString(4, customer.getId());
             return statement.executeUpdate() > 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -30,24 +55,13 @@ public class CustomerController implements CustomerServices {
     }
 
     @Override
-    public boolean updateCustomer(Customer customer) {
-        PreparedStatement statement = null;
-        try {
-            statement = DBConnection.getInstance().getConnection().prepareStatement("UPDATE customer SET name=?,address=?,salary=? WHERE id=?");
-            statement.setString(1, customer.getName());
-            statement.setString(2, customer.getAddress());
-            statement.setDouble(3, customer.getSalary());
-            statement.setString(4, customer.getId());
-            return statement.executeUpdate() >0;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
     public boolean deleteCustomer(String id) {
-        try {
-            return DBConnection.getInstance().getConnection().createStatement().executeUpdate("DELETE FROM customer WHERE id = '" + id + "'") > 0;
+        String sql = "DELETE FROM customer WHERE id=?";
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, id);
+
+            return statement.executeUpdate() > 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -55,78 +69,114 @@ public class CustomerController implements CustomerServices {
 
     @Override
     public Customer searchCustomer(String id) {
-        try {
-            ResultSet res = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT * FROM customer WHERE id = '" + id + "'");
-            res.next();
-            return new Customer(
-                    res.getString(1),
-                    res.getString(2),
-                    res.getString(3),
-                    Double.parseDouble(res.getString(4))
-            );
+        String sql = "SELECT id, name, address, salary FROM customer WHERE id = ?";
+
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, id);
+            try (ResultSet res = statement.executeQuery()) {
+                if (res.next()) {
+                    return new Customer(
+                            res.getString("id"),
+                            res.getString("name"),
+                            res.getString("address"),
+                            res.getDouble("salary")
+                    );
+                } else {
+                    return null;
+                }
+            }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to search customer with ID: " + id, e);
         }
     }
+
 
     @Override
     public List<Customer> getCustomers() {
-        Connection connection = null;
-        try {
-            List<Customer> customers = new ArrayList<>();
-            ResultSet res = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT * FROM customer");
+        String sql = "SELECT id, name, address, salary FROM customer";
+        List<Customer> customers = new ArrayList<>();
+
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet res = statement.executeQuery()) {
+
             while (res.next()) {
                 customers.add(new Customer(
-                        res.getString(1),
-                        res.getString(2),
-                        res.getString(3),
-                        Double.parseDouble(res.getString(4))
+                        res.getString("id"),
+                        res.getString("name"),
+                        res.getString("address"),
+                        res.getDouble("salary")
                 ));
             }
-            return customers;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to retrieve customers", e);
         }
+        return customers;
     }
+
 
     @Override
     public String getLastId() {
-        ResultSet res = null;
-        try {
-            res = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT id FROM customer ORDER BY id DESC LIMIT 1");
-            res.next();
-            return res.getString(1);
+        String sql = "SELECT id FROM customer ORDER BY id DESC LIMIT 1";
+
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet res = statement.executeQuery(sql)) {
+
+            if (res.next()) {
+                return res.getString("id");
+            } else {
+                return null;
+            }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to retrieve the last customer ID", e);
         }
     }
+
 
     @Override
     public List<String> getCustomerIDs() {
-        try {
-            ResultSet resultSet = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT id FROM customer");
-            List<String> customerIDs = new ArrayList<>();
+        String sql = "SELECT id FROM customer";
+        List<String> customerIDs = new ArrayList<>();
+
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
             while (resultSet.next()) {
-                customerIDs.add(resultSet.getString(1));
+                customerIDs.add(resultSet.getString("id"));
             }
-            return customerIDs;
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return customerIDs;
     }
+
 
     @Override
     public String getCustomerName(String id) {
-        try {
-            ResultSet resultSet = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT name FROM customer WHERE id = '" + id + "'");
-            if (resultSet.next()) {
-                return resultSet.getString(1);
+        String sql = "SELECT name FROM customer WHERE id = ?";
+
+        try (Connection connection = DBConnection.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getString("name");
+                }
+                return null;
             }
-            return null;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to retrieve customer name", e);
         }
     }
+
 
 
 }

70de19d6b3610dd67f177075ca09ac3d14c6a6bd
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2025-01-23 10:41:40 +0530

    LoginSignupController Refactored

--- src/main/java/controller/login_signup/LoginSignupController.java
+++ src/main/java/controller/login_signup/LoginSignupController.java
@@ -1,58 +1,56 @@
 package controller.login_signup;
 
-import db.DBConnection;
 import model.User;
 import org.jasypt.util.text.BasicTextEncryptor;
+import util.CrudUtil;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginSignupController implements LoginSignupServices{
-
     private static LoginSignupServices loginSignupServices;
 
     public static LoginSignupServices getInstance() {
-        return loginSignupServices==null?loginSignupServices=new LoginSignupController():loginSignupServices;
+        if (loginSignupServices == null) {
+            loginSignupServices = new LoginSignupController();
+        }
+        return loginSignupServices;
     }
 
     @Override
     public boolean signup(User user) {
+        String sql = "INSERT INTO users (username, email, password) VALUES (?,?,?)";
         try {
-            PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement("INSERT INTO users (username, email, password) VALUES (?,?,?)");
-            statement.setString(1, user.getUsername());
-            statement.setString(2, user.getEmail());
-            statement.setString(3, user.getPassword());
-            return statement.executeUpdate()>0;
+            return CrudUtil.execute(sql, user.getUsername(), user.getEmail(), user.getPassword());
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return false;
         }
     }
 
     public boolean checkUser(String email) {
+        String sql = "SELECT * FROM users WHERE email = ?";
         try {
-            return DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT * FROM users WHERE email ='" + email + "'").next();
+            return CrudUtil.execute(sql, email);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return false;
         }
     }
 
     @Override
     public boolean login(String email, String password) {
+        String sql = "SELECT * FROM users WHERE email = ?";
         try {
-            ResultSet resultSet = DBConnection.getInstance().getConnection().createStatement().executeQuery("SELECT * FROM users WHERE email ='" + email + "'");
+            ResultSet resultSet = CrudUtil.execute(sql, email);
             if (resultSet.next()) {
                 BasicTextEncryptor basicTextEncryptor = new BasicTextEncryptor();
                 String key = "@bCd3f";
                 basicTextEncryptor.setPassword(key);
 
-                if (basicTextEncryptor.decrypt(resultSet.getString("password")).equals(password)) {
-                    return true;
-                }
+                return basicTextEncryptor.decrypt(resultSet.getString("password")).equals(password);
             }
+            return false;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return false;
         }
-        return false;
     }
 }

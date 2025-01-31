c3aecbc4a418458465d3600cd6c91e3b91477f51
NipunBasnayake <nipunsathsara1999.com>
2025-01-31 22:23:44 +0530

    Implement Dao Layer in Layered Architecture

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,15 +1,1 @@
-package dao;
 
-public class DaoFactory {
-    private static DaoFactory instance;
-
-    public static DaoFactory getInstance() {
-        if (instance == null) {
-            instance = new DaoFactory();
-        }
-        return instance;
-    }
-    private DaoFactory() {}
-
-
-}
